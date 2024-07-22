a01a4acc99c61979c373e999db2b42e2e4c5e8b1
Maksim Leskin <maksim.leskin@methed.ru>
2024-07-22 17:54:36 +0300

    fix2

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ import Swiper from "swiper";
 import "swiper/scss";
 
 new Swiper(".reviews__slider", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    620: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    980: {
+      slidesPerView: 2,
+      spaceBetween: 12,
+    },
+    1240: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
 });
